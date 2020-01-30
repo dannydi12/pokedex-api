47f5de8a7dfd6f617d33dc7dd227d96e5b605d2f
Daniel Di Venere <danieldivenere@gmail.com>
2020-01-30 11:56:20 -0800

    added environmental variables

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
 app.use(morgan('dev'));
 
-app.get('/api/test', (req, res) => {
-  res.send('got it');
-})
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+
+function handleGetTypes(req, res) {
+  res.json(validTypes)
+}
+
+app.get('/types', handleGetTypes)
 
 module.exports = {
   app
