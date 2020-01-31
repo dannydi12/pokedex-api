f429effb175ca1d09f51de28d44dbd6eb6c41484
Daniel Di Venere <danieldivenere@gmail.com>
2020-01-31 10:32:47 -0800

    prepared for deployment

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
+const POKEDEX = require('./pokedex.json');
 
 const app = express();
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting));
+
+app.use(function validateBearerToken(req, res, next) {
+  if (req.headers.authorization !== process.env.API_KEY) {
+    res.status(401).send('Not a valid key');
+  }
+  next();
+});
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
 
@@ -12,6 +30,13 @@ function handleGetTypes(req, res) {
 }
 
 app.get('/types', handleGetTypes)
+
+
+function handleGetPokemon(req, res) {
+  res.send('Hello Pokemon!')
+}
+
+app.get('/pokemon', handleGetPokemon)
 
 module.exports = {
   app
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const {app} = require('./index');
 
-app.listen(8000, () => {
-  console.log('Listening on port 8000...');
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
 })
+
